3fabedce3774c584e77597cb646cf84268e3a4cd
Michael Mulich <michael.mulich@gmail.com>
2025-11-28 13:06:35 -0800

    Rename function to prevent misinterpretation

--- django_mitre/core/utils/url.py
+++ django_mitre/core/utils/url.py
@@ -10,12 +10,12 @@ from .model import model_url_name
 
 
 __all__ = [
-    "get_view_name",
+    "get_view_class_name",
     "produce_paths_for_model",
 ]
 
 
-def get_view_name(m: Model, v: str):
+def get_view_class_name(m: Model, v: str):
     """Produce the view name from the model and view indicator"""
     return f"{m.__name__}{v.title()}View"
 
@@ -45,7 +45,7 @@ def produce_paths_for_model(
     assert views, f"Missing `views` module for {app_config.name}"
 
     # Get or create the index view class.
-    index_view_class = getattr(views, get_view_name(model, "index"), None)
+    index_view_class = getattr(views, get_view_class_name(model, "index"), None)
     if index_view_class is None:
         index_view_class = IndexView
 
@@ -60,7 +60,7 @@ def produce_paths_for_model(
     # Create views
     filter_view = FilterView.as_view(model=model, filterset_class=filterset_class)
     index_view = index_view_class.as_view(model=model, filterset_class=filterset_class)
-    detail_view = getattr(views, get_view_name(model, "detail")).as_view()
+    detail_view = getattr(views, get_view_class_name(model, "detail")).as_view()
 
     paths = [
         re_path(
